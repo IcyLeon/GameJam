eb102109e7928122ea367b8ba1a2e1f3b46dc111
Randall <88996884+IcyLeon@users.noreply.github.com>
2023-10-12 19:12:39 +0800

    Improve rendering of tiles

--- Assets/Script/Player&NPC/NPC.cs
+++ Assets/Script/Player&NPC/NPC.cs
@@ -72,6 +72,8 @@ public class NPC : MoveableObjects
     protected override void Update()
     {
         UpdateState();
+        base.Update();
+        
     }
 
     private void SetupItemImages()
@@ -88,7 +90,7 @@ public class NPC : MoveableObjects
         }
     }
 
-    private void UpdateState()
+    protected override void UpdateState()
     {
         switch(state)
         {
